b6cae0f375f4a8324547164038e3c6845fe11fff
ChuangZiyan <ziyan.zhuang@h3platform.com>
2023-10-24 15:24:21 +0800

    加入專案檔案。

---
+++ Demo_WinFormsApp/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Run_Button = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 19
+        Me.ListBox1.Location = New System.Drawing.Point(12, 37)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(248, 346)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Run_Button
+        '
+        Me.Run_Button.Location = New System.Drawing.Point(12, 409)
+        Me.Run_Button.Name = "Run_Button"
+        Me.Run_Button.Size = New System.Drawing.Size(94, 29)
+        Me.Run_Button.TabIndex = 2
+        Me.Run_Button.Text = "Run"
+        Me.Run_Button.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(266, 37)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(410, 346)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(688, 450)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Run_Button)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Run_Button As Button
+    Friend WithEvents ListView1 As ListView
+End Class
---
+++ Demo_WinFormsApp/Form1.vb
@@ -0,0 +1,113 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Init_ListView()
+        Init_ListBox()
+    End Sub
+
+
+
+    Private Sub Init_ListView()
+        ListView1.View = View.Details
+        ListView1.GridLines = True
+        ListView1.FullRowSelect = True
+        ListView1.Columns.Add("Columns 1", 135)
+        ListView1.Columns.Add("Columns 2", 135)
+        ListView1.Columns.Add("Columns 3", 135)
+
+    End Sub
+
+
+    Private Sub Init_ListBox()
+
+        ListBox1.Items.Add("Item A")
+        ListBox1.Items.Add("Item B")
+        ListBox1.Items.Add("Item C")
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        Dim selectedItemText As String = ListBox1.SelectedItem.ToString()
+        ListView1.Items.Clear()
+
+        If selectedItemText = "Item A" Then
+
+            For i = 1 To 4
+                ListView1.Items.Add("A-" & i & "-1")
+                For j = 2 To 3
+                    ListView1.Items(i - 1).SubItems.Add("A-" & i & "-" & j)
+                Next
+            Next
+
+        ElseIf selectedItemText = "Item B" Then
+            For i = 1 To 3
+                ListView1.Items.Add("B-" & i & "-1")
+                For j = 2 To 3
+                    ListView1.Items(i - 1).SubItems.Add("B-" & i & "-" & j)
+                Next
+            Next
+
+        ElseIf selectedItemText = "Item C" Then
+            For i = 1 To 6
+                ListView1.Items.Add("C-" & i & "-1")
+                For j = 2 To 3
+                    ListView1.Items(i - 1).SubItems.Add("C-" & i & "-" & j)
+                Next
+            Next
+        End If
+
+
+    End Sub
+
+    Private Async Sub Run_Button_Click(sender As Object, e As EventArgs) Handles Run_Button.Click
+
+        Dim Selected_idx = 0
+        Dim Max_Listview_item = 0
+
+        While True
+
+            For i = 0 To ListBox1.Items.Count - 1
+                ListBox1.SelectedIndex = i
+                Await Delay_msec(500)
+
+                ListView1.Focus()
+
+                If ListView1.Items.Count > Max_Listview_item Then
+                    Max_Listview_item = ListView1.Items.Count
+                End If
+
+                If ListView1.Items.Count > Selected_idx Then
+                    ListView1.Items(Selected_idx).Selected = True
+
+                    Your_sub() ' 改成你要的事件
+                End If
+
+                Await Delay_msec(500)
+
+            Next
+
+            Selected_idx += 1
+
+            If Selected_idx = Max_Listview_item Then
+                Exit While
+            End If
+
+        End While
+
+        MsgBox("Done")
+
+    End Sub
+
+
+
+    Private Sub Your_sub()
+        ' add your code here
+        Debug.WriteLine(ListView1.SelectedItems.Item(0).ToString())
+    End Sub
+
+
+
+    Public Shared Async Function Delay_msec(msec As Integer) As Task
+        Await Task.Delay(msec)
+    End Function
+End Class
